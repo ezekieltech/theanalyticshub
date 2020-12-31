6bb53766ef3d6dc42e383c8c710bed475976c998
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2020-12-31 09:45:26 +0100

    Fix minor changes

--- theanalyticshub/urls.py
+++ theanalyticshub/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     url(r'', include('mainWebsite.urls')),
     url(r'^profile/', include('professionals.urls')),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
