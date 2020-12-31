d514cfab79972320a61532c6788880121d0dc068
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2020-12-31 09:48:05 +0100

    Fix minor changes

--- theanalyticshub/urls.py
+++ theanalyticshub/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
